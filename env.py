5c608c277f40277c48578404670d9896dcad9f02
gnurt2041 <trungrus1402@gmail.com>
2023-12-16 08:26:26 +0700

    Update file path

--- env.py
+++ env.py
@@ -4,7 +4,7 @@ ASSETS_PATH = Path(__file__).parent/ 'assets'
 
 FONTS_ROOT = Path(f'{ASSETS_PATH}/font/static')
 MANGA109_ROOT = Path('data/Manga109s_released_2021_12_30')
-DATA_ROOT = Path('./')
+DATA_ROOT = Path('./data/images_text')
 BACKGROUND_DIR = Path(f'{MANGA109_ROOT}/background')
 DATA_SYNTHETIC_ROOT = Path(f'{MANGA109_ROOT}/synthetic')
 TRAIN_ROOT = Path('./out')
